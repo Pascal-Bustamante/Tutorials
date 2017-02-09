08bbe75e5b0fc362cec907ae461cf79d557a77d8
Pascal-Bustamante <pascalbustamante@utexas.edu>
2017-02-08 22:24:03 -0600

    s
    s

---
+++ Lague's tutorial shooter/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerControler : MonoBehaviour {
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrcetedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrcetedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}

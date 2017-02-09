c9946d4c599fbd9f9e43cd357af99e2758e373fc
Pascal-Bustamante <pascalbustamante@utexas.edu>
2017-02-09 17:11:59 -0600

    s
    s

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent pathfinder;
+    Transform target;
+
+	void Start () {
+        pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(UpdatePath ());
+	}
+	
+	void Update () {
+ 	}
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerControler))]
+[RequireComponent (typeof (GunController))]
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 5;
+
+    Camera viewCamera;
+    PlayerControler controller;
+    GunController gunController;
+
+	void Start () {
+        controller = GetComponent<PlayerControler> ();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+	}
+	
+	void Update () {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move (moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray,out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            //Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
+        }
+
+	}
+}
